b5f474665508643a9ece9ae7ef93b065ae3bd371
love-quinn <63768468+love-quinn@users.noreply.github.com>
2024-11-05 22:08:58 -0300

    notication sound added + separator + prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -3,64 +3,116 @@
 import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 import GaugeCircle from "@/components/ui/gauge-circle";
-import { PlusIcon } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { GlassWater, PlusIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function Home() {
-  const imageUrl = `https://images.unsplash.com/photo-1548780607-46c78f38182d?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+  const imageUrl = `https://images.unsplash.com/photo-1548780607-46c78f38182d?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
   const [value, setValue] = useState(0);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+  const bellSound = new Audio("/bell_ring.mp3");
 
   const requestNotificationPermission = async (): Promise<boolean> => {
     if (!("Notification" in window)) {
       console.error("Navegador não suporta notificações");
       return false;
     }
-  
+
     if (Notification.permission === "granted") return true;
-  
+
     const permission = await Notification.requestPermission();
     return permission === "granted";
+  };
+
+  const playBellSoundThreeTimes = () => {
+    let playCount = 0;
+
+    const handlePlayEnd = () => {
+      playCount++;
+
+      if (playCount < 1) {
+        bellSound.play();
+      } else {
+        bellSound.removeEventListener("ended", handlePlayEnd);
+      }
+    };
+
+    if (bellSound) {
+      bellSound.play();
+      bellSound.addEventListener("ended", handlePlayEnd);
+    }
   };
 
   const sendHydrationReminderNotification = () => {
     const notification = new Notification("Hora de beber água!", {
       body: "Lembre-se de beber mais 200ml de água.",
       icon: imageUrl,
-      // icon: "https://unsplash.it/400/400",
     });
-    
     setTimeout(() => notification.close(), 3000);
   };
 
   const handleIncrement = async () => {
     setValue((prev) => (prev < 2000 ? prev + 200 : prev));
-  
+
     if (await requestNotificationPermission()) {
-      setTimeout(sendHydrationReminderNotification, 3600000); // Lembrete em 1 hora
+      if (timeoutId) clearTimeout(timeoutId);
+
+      const newTimeoutId = setTimeout(() => {
+        sendHydrationReminderNotification();
+        playBellSoundThreeTimes();
+      }, 3600 * 1000);
+
+      setTimeoutId(newTimeoutId);
     }
   };
 
+  const primaryColor = value >= 2000 ? "rgb(34, 197, 94)" : "rgb(79, 70, 229)";
+
   return (
-    <div className="flex flex-col w-full h-screen items-center">
+    <div className="flex h-screen w-full flex-col items-center">
       <Header />
 
       {/* Center */}
-      <div className="flex flex-col items-center gap-10 justify-center flex-grow">
-        <GaugeCircle
-          max={2000}
-          min={0}
-          value={value}
-          gaugePrimaryColor="rgb(79 70 229)"
-          gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
-        />
-        <Button
-          onClick={handleIncrement}
-          className="gap-1 bg-[#4f46e5] hover:bg-[#4e46e5de]"
-        >
-          <PlusIcon />
-          200ml
-        </Button>
+      <div
+        className={`flex w-full flex-grow flex-col items-center justify-center gap-10`}
+      >
+        <div className="mt-12 flex flex-none flex-col items-center justify-center gap-10">
+          <GaugeCircle
+            max={2000}
+            min={0}
+            value={value}
+            gaugePrimaryColor={primaryColor}
+            gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
+          />
+          <Button
+            onClick={handleIncrement}
+            className={`${
+              value >= 2000
+                ? "bg-green-500 hover:bg-green-600"
+                : "bg-[#4f46e5] hover:bg-[#4e46e5de]"
+            } gap-1`}
+          >
+            <PlusIcon />
+            200ml
+          </Button>
+        </div>
 
+        <div className="mt-12 flex w-1/3 flex-col gap-3 justify-between text-gray-500">
+          <h4 className="leading-none text-sm">Daily records</h4>
+          <Separator/>
+
+          <div className="flex justify-between mt-1">
+
+          <div className="flex items-center gap-1">
+            <GlassWater size={16} className="" />
+            <p className="text-xs">+200ml</p>
+          </div>
+
+          <p className="text-xs">9:40PM</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
