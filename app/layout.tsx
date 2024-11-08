9122587b3f41b715974f28c56b3349b508449cdb
love-quinn <63768468+love-quinn@users.noreply.github.com>
2024-11-07 23:01:52 -0300

    feat: updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Poppins } from "next/font/google"
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400","500", "600", "700","800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
 
 export const metadata: Metadata = {
   title: "Waterfy",
   description: "Your water reminder web app!",
+  icons: {
+    icon: {
+      url: "/images/favicon-32x32.png",
+      href: "/images/favicon-32x32.png",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -20,11 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
